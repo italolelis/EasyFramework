1a9374aee91723fe55e2585d3c0efbcea11766f1
Italo <Italo@192.168.2.2>
2011-11-01 17:42:07 -0200

    Better documentation

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -7,9 +7,9 @@
  *  @copyright Copyright 2011, EasyFramework (http://www.easy.lellysinformatica.com)
  *
  */
-/* Inclui as classes baicas */
+/* Basic classes */
 require_once "common/basics.php";
-/* Inclui as classes de funcionamento */
+/* Core classes */
 App::import("Core", array(
     "common/class_registry",
     "common/filesystem",
@@ -26,9 +26,9 @@ App::import("Core", array(
     "storage/session",
     "view/view"
 ));
-/* Inclui a biblioteca smarty */
+/* Import the Smarty's lib */
 App::import("Lib", "smarty/Smarty.class");
-/* Inclui as configurações da app */
+/* Import the app configs */
 App::import('Config', array('functions', 'database', 'settings'));
 /*  Inclusão das classes da biblioteca do EasyFramework ou das classes as sobrescrevem */
 App::import("Controller", "app_controller");
