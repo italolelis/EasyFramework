6f4c8a81ed77e99333e4d136af09029f2b5ddc55
Italo <Italo@192.168.2.2>
2011-12-14 03:34:08 -0200

    Demo adpatation to core version 0.2.12

--- demo/app/config/settings.php
+++ demo/app/config/settings.php
@@ -11,21 +11,121 @@
  */
 Mapper::root("home");
 /**
+ * Turn off all caching application-wide.
+ *
+ */
+//Configure::write('Cache.disable', true);
+
+/**
  *  Template é onde você poderá configurar o comportamento do seu template(views).
  *  Nele podem ser setadas configurações como: Cacheble: se um template será guardado
  *  em cache, Urls: Define as urls que serão passadas para a view.
  */
-Config::write("template", array(
-    "layout" => array(
-        "layout" => "layout.tpl"
-    ),
-    //Configuramos as urls que serão usuadas nas views
-    "urls" => array(
-        'home' => 'home',
-        'usuarios' => 'usuarios',
-        'incluirUsuario' => 'usuarios/incluir',
-        'editarUsuario' => 'usuarios/edit/',
-        'excluirUsuario' => 'usuarios/excluir/',
-    )
+Config::write("template.layouts", array(
+    "layout" => "layout.tpl"
+));
+
+Config::write("template.cache", array(
+    "cache" => false, //Cache desabilitado
+    "time" => 3600
+));
+//Configuramos as urls que serão usuadas nas views
+Config::write("template.urls", array(
+    'home' => 'home',
+    'usuarios' => 'usuarios',
+    'incluirUsuario' => 'usuarios/incluir',
+    'editarUsuario' => 'usuarios/edit/',
+    'excluirUsuario' => 'usuarios/excluir/',
+));
+
+
+/**
+ *
+ * Cache Engine Configuration
+ * Default settings provided below
+ *
+ * File storage engine.
+ *
+ * 	 Cache::config('default', array(
+ * 		'engine' => 'File', //[required]
+ * 		'duration'=> 3600, //[optional]
+ * 		'probability'=> 100, //[optional]
+ * 		'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
+ * 		'prefix' => 'easy_', //[optional]  prefix every cache file with this string
+ * 		'lock' => false, //[optional]  use file locking
+ * 		'serialize' => true, [optional]
+ * 	));
+ *
+ * APC (http://pecl.php.net/package/APC)
+ *
+ * 	 Cache::config('default', array(
+ * 		'engine' => 'Apc', //[required]
+ * 		'duration'=> 3600, //[optional]
+ * 		'probability'=> 100, //[optional]
+ * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ * 	));
+ *
+ * Xcache (http://xcache.lighttpd.net/)
+ *
+ * 	 Cache::config('default', array(
+ * 		'engine' => 'Xcache', //[required]
+ * 		'duration'=> 3600, //[optional]
+ * 		'probability'=> 100, //[optional]
+ * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional] prefix every cache file with this string
+ * 		'user' => 'user', //user from xcache.admin.user settings
+ * 		'password' => 'password', //plaintext password (xcache.admin.pass)
+ * 	));
+ *
+ * Memcache (http://www.danga.com/memcached/)
+ *
+ * 	 Cache::config('default', array(
+ * 		'engine' => 'Memcache', //[required]
+ * 		'duration'=> 3600, //[optional]
+ * 		'probability'=> 100, //[optional]
+ * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ * 		'servers' => array(
+ * 			'127.0.0.1:11211' // localhost, default port 11211
+ * 		), //[optional]
+ * 		'persistent' => true, // [optional] set this to false for non-persistent connections
+ * 		'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
+ * 	));
+ *
+ *  Wincache (http://php.net/wincache)
+ *
+ * 	 Cache::config('default', array(
+ * 		'engine' => 'Wincache', //[required]
+ * 		'duration'=> 3600, //[optional]
+ * 		'probability'=> 100, //[optional]
+ * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ * 	));
+ */
+$engine = 'File';
+// In development mode, caches should expire quickly.
+$duration = 3600;
+if (Config::read('debug')) {
+    $duration = '+10 seconds';
+}
+
+// Setup a 'default' cache configuration for use in the application.
+Cache::config('default', array(
+    'engine' => $engine, //[required]
+    'duration' => $duration, //[optional]
+    'probability' => 100, //[optional]
+    'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
+    'prefix' => 'easy_', //[optional]  prefix every cache file with this string
+    'lock' => false, //[optional]  use file locking
+    'serialize' => true,
+));
+
+/**
+ * Configure the cache for model and datasource caches.  This cache configuration
+ * is used to store schema descriptions, and table listings in connections.
+ */
+Cache::config('_easy_model_', array(
+    'engine' => $engine,
+    'prefix' => 'easy_model_',
+    'path' => CACHE . 'models' . DS,
+    'serialize' => ($engine === 'File'),
+    'duration' => $duration
 ));
 ?>
