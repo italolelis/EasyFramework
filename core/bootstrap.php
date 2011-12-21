9cac3f69eed15e843f8edb049829d5f017194313
Italo <Italo@192.168.2.2>
2011-12-21 13:48:47 -0200

    New bootstrap system

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -11,86 +11,87 @@
  * Path to the temporary files directory.
  */
 if (!defined('TMP')) {
-    define('TMP', APP_PATH . 'webroot/tmp' . DS);
+    define('TMP', APP_PATH . 'tmp' . DS);
 }
 
 /**
  * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
  */
 define('CACHE', TMP . 'cache' . DS);
+/**
+ * Path to the log files directory. It can be shared between hosts in a multi-server setup.
+ */
+define('LOGS', TMP . 'logs' . DS);
 
 /* Basic classes */
-require_once "Common/App.php";
+require CORE . 'Common' . DS . 'App.php';
+require CORE . 'Debug' . DS . 'Exceptions.php';
 
+spl_autoload_register(array('App', 'load'));
 App::build();
-//spl_autoload_register(array('App', 'load'));
-//
-//App::uses('AnnotationManager', 'Annotation');
-//App::uses('Cache', 'Cache');
-//App::uses('Config', 'Ccommon');
-//App::uses('enum', 'Common');
-//App::uses('Hookable', 'Common');
-//App::uses('validation', 'Common');
-//App::uses('Inflector', 'Common');
-//App::uses('ClassRegistry', 'Utility');
-//App::uses('Utility/FileSystem', 'Core');
-//App::uses('Controller', 'Controller');
-//App::uses('Debug', 'Core/debug');
-//
-//App::uses('Dispatcher', 'Dispatcher');
-//
-//App::uses('Mapper', 'Dispatcher');
-//App::uses('model', 'Model');
-//App::uses('security/security', 'Core');
-//App::uses('storage/cookie', 'Core');
-//App::uses('storage/session', 'Core');
-//App::uses('view/view', 'Core');
-//
-//App::uses('smarty/Smarty.class', 'Lib');
-//
-//App::uses('functions', 'Config');
-//App::uses('database', 'Config');
-//App::uses('settings', 'Config');
-//
-//App::uses('app_controller', 'Controller');
-//App::uses('app_model', 'Model');
 
-/* Core classes */
-App::import("Core", array(
-    "Annotations/AnnotationManager",
-    //Cache System
-    "Cache/Cache",
-    //Common Files
-    "Common/Config",
-    "Common/Enum",
-    "Common/Hookable",
-    "Common/Validation",
-    "Common/Inflector",
-    //Ultility classes
-    "Utility/ClassRegistry",
-    "Utility/FileSystem",
-    //Controller Manager
-    "Controller/Controller",
-    //Debug System
-    "Debug/Debug",
-    //Dispatcher System
-    "Dispatcher/Dispatcher",
-    "Dispatcher/Mapper",
-    //Model Manager
-    "Model/Model",
-    //Security System
-    "Security/Security",
-    //Storage System
-    "Storage/Cookie",
-    "Storage/Session",
-    //View Manager
-    "View/View"
-));
-/* Import the Smarty's lib */
-App::import("Lib", "smarty/Smarty.class");
-/* Import the app configs */
-App::import('Config', array('functions', 'database', 'settings'));
-/*  Import the app base classes */
-App::import("Controller", "app_controller");
-App::import("Model", "app_model");
+App::uses('Cache', 'Core/Cache');
+App::uses('Debug', 'Core/Debug');
+App::uses('Config', 'Core/Common');
+App::uses('Enum', 'Core/Common');
+
+App::uses('Inflector', 'Core/Common');
+App::uses('ClassRegistry', 'Core/Utility');
+App::uses('FileSystem', 'Core/Utility');
+
+App::uses('Dispatcher', 'Core/Dispatcher');
+App::uses('Mapper', 'Core/Dispatcher');
+
+App::uses('Controller', 'Core/Controller');
+App::uses('Model', 'Core/Model');
+App::uses('View', 'Core/View');
+
+App::uses('Security', 'Core/Security');
+
+App::import('Config', array('database', 'settings'));
+
+App::uses('AppController', 'App/controllers');
+App::uses('AppModel', 'App/models');
+
+Debug::handleExceptions();
+Debug::handleErrors();
+
+///* Core classes */
+//App::import("Core", array(
+//    "Annotations/AnnotationManager",
+//    //Cache System
+//    "Cache/Cache",
+//    //Common Files
+//    "Common/Config",
+//    "Common/Enum",
+//    "Common/Hookable",
+//    "Common/Validation",
+//    "Common/Inflector",
+//    //Ultility classes
+//    "Utility/ClassRegistry",
+//    "Utility/FileSystem",
+//    //Controller Manager
+//    "Controller/Controller",
+//    //Debug System
+//    "Debug/Debug",
+//    //Dispatcher System
+//    "Dispatcher/Dispatcher",
+//    "Dispatcher/Mapper",
+//    //Model Manager
+//    "Model/Model",
+//    //Security System
+//    "Security/Security",
+//    //Storage System
+//    "Storage/Cookie",
+//    "Storage/Session",
+//    //View Manager
+//    "View/View"
+//));
+///* Import the Smarty's lib */
+//App::import("Lib", "smarty/Smarty.class");
+///* Import the app configs */
+//App::import('Config', array('functions', 'database', 'settings'));
+///*  Import the app base classes */
+//App::import("App", "controllers/app_controller");
+//App::import("App", "models/app_model");
 ?>
