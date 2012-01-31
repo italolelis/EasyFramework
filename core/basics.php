ce6c1f24a2678d0d82504fcef696833683bc7ff4
Italo <Italo@192.168.2.2>
2012-01-31 00:41:35 -0200

    Added  stripslashes_deep function which recursively strips slashes from all values in an array

--- core/basics.php
+++ core/basics.php
@@ -137,3 +137,23 @@ function env($key) {
     }
     return null;
 }
+
+/**
+ * FROM CAKEPHP
+ * 
+ * Recursively strips slashes from all values in an array
+ *
+ * @param array $values Array of values to strip slashes
+ * @return mixed What is returned from calling stripslashes
+ * @link http://book.cakephp.org/2.0/en/core-libraries/global-constants-and-functions.html#stripslashes_deep
+ */
+function stripslashes_deep($values) {
+    if (is_array($values)) {
+        foreach ($values as $key => $value) {
+            $values[$key] = stripslashes_deep($value);
+        }
+    } else {
+        $values = stripslashes($values);
+    }
+    return $values;
+}
