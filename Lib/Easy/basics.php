4270df2cfd68ac25241c0f8a896d5b1e2f65f0b7
italolelis <italolelis@lellysinforamtica.com>
2012-03-05 23:52:43 -0300

    New function for internationalization

--- Lib/Easy/basics.php
+++ Lib/Easy/basics.php
@@ -157,3 +157,26 @@ function stripslashes_deep($values) {
     }
     return $values;
 }
+
+/**
+ * Returns a translated string if one is found; Otherwise, the submitted message.
+ *
+ * @param string $singular Text to translate
+ * @param mixed $args Array with arguments or multiple arguments in function
+ * @return mixed translated string
+ * @link http://book.cakephp.org/2.0/en/core-libraries/global-constants-and-functions.html#__
+ */
+function __($singular, $args = null) {
+    if (!$singular) {
+        return;
+    }
+
+    App::uses('I18n', 'Core/Localization');
+    $translated = I18n::translate($singular);
+    if ($args === null) {
+        return $translated;
+    } elseif (!is_array($args)) {
+        $args = array_slice(func_get_args(), 1);
+    }
+    return vsprintf($translated, $args);
+}
