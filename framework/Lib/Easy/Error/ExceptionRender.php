7ceecaba6a7a254fbc2af48df0cb7ba9366367d0
italolelis <italolelis@gmail.com>
2012-04-09 15:38:49 -0300

    Better support to User customErrors view

--- framework/Lib/Easy/Error/ExceptionRender.php
+++ framework/Lib/Easy/Error/ExceptionRender.php
@@ -2,34 +2,63 @@
 
 class ExceptionRender {
 
-    protected $exception;
+    protected $_exception;
 
     function __construct(EasyException $ex) {
-        $this->exception = $ex;
+        $this->_exception = $ex;
     }
 
-    function render() {
-        $debug = is_null(Config::read("debug")) ? false : Config::read("debug");
+    public function render() {
+        $debug = Config::read("App.debug");
+        Config::write('Error.exception', $this->_exception);
 
-        if ($debug) {
-            Config::write('Error.exception', $this->exception);
-            $details = $this->exception->getAttributes();
-            $template = strstr(get_class($this->exception), "Exception", true);
-
-            $response = new Response(array('charset' => Config::read('App.encoding')));
-            $response->statusCode($this->exception->getCode());
-            $response->send();
-
-            require_once CORE . 'Error/templates/render_error.php';
+        if (Config::read('Exception.customErrors')) {
+            return $this->renderUserErrors();
         } else {
-            if ($this->exception->getCode() == 404) {
+            return $this->renderDefaultErrors();
+        }
+    }
+
+    protected function _getTemplate($debug) {
+        if (!empty($debug)) {
+            $template = strstr(get_class($this->_exception), "Exception", true);
+        } else {
+            if ($this->_exception->getCode() == 404) {
                 $template = 'badRequest';
-            } else if ($this->exception->getCode() == 500) {
+            } else if ($this->_exception->getCode() == 500) {
                 $template = 'serverError';
             }
+        }
+        return $template;
+    }
+
+    public function renderDefaultErrors() {
+        $debug = Config::read("App.debug");
+
+
+        $details = $this->_exception->getAttributes();
+        $template = $this->_getTemplate($debug);
+
+        $response = new Response(array('charset' => Config::read('App.encoding')));
+        $response->statusCode($this->_exception->getCode());
+        $response->send();
+
+        require_once CORE . 'Error' . DS . 'templates' . DS . 'render_error.php';
+    }
+
+    public function renderUserErrors() {
+        $template = $this->_getTemplate(Config::read("App.debug"));
+        try {
             $request = new Request('Error/' . $template);
             $dispatcher = new Dispatcher ();
-            $dispatcher->dispatch($request, new Response(array('charset' => Config::read('App.encoding'))));
+            $dispatcher->dispatch(
+                    $request, new Response(array('charset' => Config::read('App.encoding'))
+            ));
+        } catch (EasyException $exc) {
+            echo '<h3>Render Custom User Error Problem.</h3>' .
+            'Message: ' . $exc->getMessage() . ' </br>' .
+            'File: ' . $exc->getFile() . '</br>' .
+            'Line: ' . $exc->getLine();
         }
     }
 
