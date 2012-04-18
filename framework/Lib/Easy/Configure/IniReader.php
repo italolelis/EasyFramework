b4335efefa3634e5c157a4e8c1e5f1742a8469c6
italolelis <italolelis@gmail.com>
2012-04-18 01:22:38 -0300

    Updated website url

--- framework/Lib/Easy/Configure/IniReader.php
+++ framework/Lib/Easy/Configure/IniReader.php
@@ -4,12 +4,12 @@
  * FROM CAKEPHP
  * 
  * EasyFramework : Rapid Development Framework
- * Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ * Copyright 2011, EasyFramework (http://easyframework.org.br)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ * @copyright     Copyright 2011, EasyFramework (http://easyframework.org.br)
  * @since         EasyFramework v 1.5.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
--- framework/Lib/Easy/Configure/YamlReader.php
+++ framework/Lib/Easy/Configure/YamlReader.php
@@ -2,12 +2,12 @@
 
 /**
  * EasyFramework : Rapid Development Framework
- * Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ * Copyright 2011, EasyFramework (http://easyframework.org.br)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ * @copyright     Copyright 2011, EasyFramework (http://easyframework.org.br)
  * @package       app
  * @since         EasyFramework v 1.5.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
