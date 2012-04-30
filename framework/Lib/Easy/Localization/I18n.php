5cbf3ddd569c83a8118d342e7d958fc4e2564181
italolelis <italolelis@gmail.com>
2012-04-30 15:45:04 -0300

    using Hash API instead Set

--- framework/Lib/Easy/Localization/I18n.php
+++ framework/Lib/Easy/Localization/I18n.php
@@ -21,6 +21,7 @@
  * Included libraries.
  */
 App::uses('Session', 'Storage');
+App::uses('Hash', 'Utility');
 App::uses('L10n', 'Localization');
 App::uses('Multibyte', 'Localization');
 
@@ -373,7 +374,7 @@ class I18n {
                 $this->_domains[$domain][$this->_lang][$this->category]["%plural-c"] = $switch;
                 unset($this->_domains[$domain][$this->_lang][$this->category]["%po-header"]);
             }
-            $this->_domains = Set::pushDiff($this->_domains, $merge);
+            $this->_domains = Hash::mergeDiff($this->_domains, $merge);
 
             if (isset($this->_domains[$domain][$this->_lang][$this->category][null])) {
                 unset($this->_domains[$domain][$this->_lang][$this->category][null]);
