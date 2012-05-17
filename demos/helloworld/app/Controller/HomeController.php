f0f18c58769dbe01bb07b7a2642e5272962503e9
italolelis <italolelis@gmail.com>
2012-05-17 03:19:04 -0300

    New demo version 1.6.0

--- demos/helloworld/app/Controller/HomeController.php
+++ demos/helloworld/app/Controller/HomeController.php
@@ -1,13 +1,12 @@
 <?php
 
-class IndexController extends AppController {
+class HomeController extends AppController {
 
     /**
      * Used to tell the framework that in this controller we aren't using any model class
      * @var array 
      */
     public $uses = array();
-    public $modelClass = false;
 
     function index() {
         //Passing the $var var to the view
