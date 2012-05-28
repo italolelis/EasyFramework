f0fd49ec681675e1fcca32bcdb5c57d390c1178f
italolelis <italolelis@gmail.com>
2012-05-28 01:09:08 -0300

    Refectored Code

--- framework/Lib/Easy/bootstrap.php
+++ framework/Lib/Easy/bootstrap.php
@@ -11,19 +11,6 @@
  * @since         EasyFramework v 0.1
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-/* Path to the temporary files directory. */
-if (!defined('TMP')) {
-    define('TMP', APP_PATH . 'tmp' . DS);
-}
-/* Path to the cache files directory. It can be shared between hosts in a multi-server setup. */
-if (!defined('CACHE')) {
-    define('CACHE', TMP . 'cache' . DS);
-}
-/* Path to the log files directory. It can be shared between hosts in a multi-server setup. */
-if (!defined('LOGS')) {
-    define('LOGS', TMP . 'logs' . DS);
-}
-
 if (!defined('EASY_CORE_INCLUDE_PATH')) {
     define('EASY_CORE_INCLUDE_PATH', dirname(__FILE__));
 }
@@ -32,6 +19,26 @@ if (!defined('CORE')) {
     define('CORE', EASY_CORE_INCLUDE_PATH . DS);
 }
 
+/**
+ * Path to the temporary files directory.
+ */
+if (!defined('TMP')) {
+    define('TMP', APP_PATH . 'tmp' . DS);
+}
+
+/**
+ * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
+ */
+if (!defined('CACHE')) {
+    define('CACHE', TMP . 'cache' . DS);
+}
+
+/**
+ * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
+ */
+if (!defined('LOGS')) {
+    define('LOGS', TMP . 'logs' . DS);
+}
 
 /* Basic classes */
 require CORE . 'basics.php';
