cfac5c9faa9265b98860fce54b2b523a5f9dda02
italolelis <italolelis@gmail.com>
2012-06-08 15:44:57 -0300

    New Generics System
    Simulates a Generic Typed system like ASP, better to work with
    Collections

---
+++ framework/Lib/Easy/Generics/IEquatable.php
@@ -0,0 +1,12 @@
+<?
+
+/*
+ * @author Pulni4kiya <beli4ko.debeli4ko@gmail.com>
+ * @date 2009-01-03
+ * @version 1.0 2009-01-03
+ */
+interface IEquatable
+{
+
+    public function Equals($object);
+}
---
+++ framework/Lib/Easy/Generics/IGeneric.php
@@ -0,0 +1,14 @@
+<?
+
+/*
+ * @author Pulni4kiya <beli4ko.debeli4ko@gmail.com>
+ * @date 2009-03-01
+ * @version 1.0 2009-03-01
+ */
+interface IGeneric
+{
+
+    public static function NumberOfTypes();
+
+    public function GetTypes();
+}
