3c23a27b2a92799bd1264357e1be761b2dd9b77f
italolelis <italolelis@gmail.com>
2012-08-29 02:01:03 -0300

    Demo Folder Reestructure

---
+++ demos/helloworld/App/Config/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This is cache configuration file.
+ *
+ * Use it to configure core behavior of EasyFW.
+ *
+ * PHP 5
+ *
+ * EasyFramework : Rapid Development Framework
+ * Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2011, EasyFramework (http://easy.lellysinformatica.com)
+ * @package       app
+ * @since         EasyFramework v 0.3
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+//Set the Config Engine that the app will use
+$config['configEngine'] = 'yaml';
