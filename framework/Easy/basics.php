79f0988a388331a93c3550f80590765fefefc7d7
italolelis <italolelis@gmail.com>
2012-09-12 18:02:54 -0300

    Copyright update

--- framework/Easy/basics.php
+++ framework/Easy/basics.php
@@ -8,12 +8,12 @@
  * PHP 5
  *
  * EasyFramework : Rapid Development Framework
- * Copyright 2011, EasyFramework (http://easyframework.org.br)
+ * Copyright 2011, EasyFramework (http://easyframework.net)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2011, EasyFramework (http://easyframework.org.br)
+ * @copyright     Copyright 2011, EasyFramework (http://easyframework.net)
  * @since         EasyFramework v 0.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
