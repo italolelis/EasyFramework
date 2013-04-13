872268348250c586e5bdc58ad51084f553b86aab
italolelis <italolelis@gmail.com>
2013-04-13 20:03:56 -0300

    Updated docs

--- src/Easy/Mvc/Controller/Component/Locale.php
+++ src/Easy/Mvc/Controller/Component/Locale.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * This file is part of the Easy Framework package.
- *
- * (c) Ítalo Lelis de Vietro <italolelis@lellysinformatica.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+// Copyright (c) Lellys Informática. All rights reserved. See License.txt in the project root for license information.
 
 namespace Easy\Mvc\Controller\Component;
 
